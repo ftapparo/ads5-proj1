c043e8d571b53c9c7929669232576cb467e1d1db
FLAVIOTAPPAAB6C\flaviotapparo <tapparo.flavio@gmail.com>
2025-02-27 19:58:30 -0300

    Retira da mensagem do console.

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ function calcularIdade(){
     let anoDigitado = 
     document.getElementById("anoNascimento").value;
     let idade = 2025 - anoDigitado;
-    console.log(idade);
+    //console.log(idade);
     document.getElementById("resultado").innerHTML =
     `Sua idade é ${idade}`;
 }
